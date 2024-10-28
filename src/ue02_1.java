98d221ecce17ae5f80ffc369deb212a6844ec754
Lukas Weineis <S2410238058students.fh-hagenberg.at>
2024-10-28 17:16:46 +0100

    Erste Übung abgeschlossen

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,34 @@
-import libraries.Out;
+/*
+Erstellen Sie ein Java-Programm, welches drei ganze Zahlen (int) einliest und
+das Maximum der drei Werte ausgibt.
+ */
+
+import java.util.Scanner;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        System.out.println("Bitte geben Sie drei ganze Zahlen ein.\n");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Erste Zahl eingeben: ");
+        int ersteZahl = myScanner.nextInt();
+
+        System.out.println("Zweite Zahl eingeben: ");
+        int zweiteZahl = myScanner.nextInt();       
+
+        System.out.println("Dritte Zahl eingeben: ");
+        int dritteZahl = myScanner.nextInt();
+
+        myScanner.close();
+
+        if (ersteZahl > zweiteZahl && ersteZahl > dritteZahl){
+            System.out.println("Die erste Zahl " + ersteZahl + " ist die Größte!");
+        } else if (zweiteZahl > ersteZahl && zweiteZahl > dritteZahl){
+            System.out.println("Die zweite Zahl " + zweiteZahl + " ist die Größte!");
+        } else if (dritteZahl > ersteZahl && dritteZahl > zweiteZahl){
+            System.out.println("Die dritte Zahl " + dritteZahl + " ist die Größte!");
+        } else {
+            System.out.println("Alle Zahlen " + ersteZahl + " " + zweiteZahl + " " + dritteZahl + " sind gleich Groß!");
+        }
     }
 }
