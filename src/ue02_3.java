4b9b9274f22fe13198c9ecc78435fd7c225dd3fa
Lukas Weineis <S2410238058students.fh-hagenberg.at>
2024-10-29 15:51:39 +0100

    Started exercise 3

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,3 +1,14 @@
+/*
+Gegeben ist ein Kreis mit dem Radius r = 4 und dem Mittelpunkt (0,0)
+
+Schreiben Sie ein Java-Programm, das die x- und y-Koordinaten (double)
+eines Punktes einliest und feststellt, ob der Punkt innerhalb der grünen Fläche
+liegt.
+Überlegen Sie eine sinnvolle Abfolge der zu prüfenden Bedingungen.
+Anmerkung: √𝑥 = Math.sqrt(x)
+
+ */
+
 import libraries.Out;
 
 public class ue02_3 {
